a0ec6c8b1909496711ed3b3871f6ea166bec5021
sbenstewart <sbenstewart@gmail.com>
2019-04-20 16:20:46 +0530

    finally

---
+++ workshop/inc/test.php
@@ -0,0 +1,31 @@
+<?php
+
+require_once "Mail.php";
+
+$from = "Ben <sender@example.com>";
+
+$to = "Ramona Recipient <recipient@example.com>";
+
+$subject = "Hi!";
+$body = "Hi,\n\nHow are you?";
+$host = "mail.example.com";
+
+$username = "smtp_username";
+
+$password = "smtp_password";
+
+$headers = array ('From' => $from,
+'To' => $to,
+'Subject' => $subject);
+$smtp = Mail::factory('smtp',
+array ('host' => $host,
+'auth' => true,
+'username' => $username,
+'password' => $password));
+$mail = $smtp->send($to, $headers, $body);
+if (PEAR::isError($mail)) {
+echo("<p>" . $mail->getMessage() . "</p>");
+} else {
+echo("<p>Message successfully sent!</p>");
+}
+?>
