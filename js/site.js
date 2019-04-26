09b26d5b83a7fbfa42df3683bdbdc0c6c09002c9
sbenstewart <sbenstewart@gmail.com>
2019-04-26 20:26:21 +0530

    service worker added

--- js/site.js
+++ js/site.js
@@ -1,27 +1,3 @@
-const publicKey = 'BNhxhvXamm6Agl8V03vfHyp86ggCvgSBDWLuDtGMwez_EMrH0IG5A1xZpI4zFBmT-L9uCXW0rGAtLMJEpTGMTjM';
-
 navigator.serviceWorker && navigator.serviceWorker.register('./js/sw.js').then(function(registration) {
   console.log('Excellent, registered with scope: ', registration.scope);
-});
-
-navigator.serviceWorker && navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
-  serviceWorkerRegistration.pushManager.getSubscription()
-    .then(function(subscription) {
-      // subscription will be null or a PushSubscription
-      if (subscription) {
-        console.info('Got existing', subscription);
-        window.subscription = subscription;
-        return;  // got one, yay
-      }
-
-      const applicationServerKey = urlB64ToUint8Array(publicKey);
-      serviceWorkerRegistration.pushManager.subscribe({
-          userVisibleOnly: true,
-          applicationServerKey,
-      })
-        .then(function(subscription) {
-          console.info('Newly subscribed to push!', subscription);
-          window.subscription = subscription;
-        });
-    });
 });
--- js/sw.js
+++ js/sw.js
@@ -2,10 +2,3 @@
 self.addEventListener('fetch', function(event) {
   /** An empty fetch handler! */
 });
-
-self.addEventListener('push', function(event) {
-  event.waitUntil(
-    self.registration.showNotification('Got Push?', {
-      body: 'Push Message received'
-   }));
-});
