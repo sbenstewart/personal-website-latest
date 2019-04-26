25f73ab4169a718dc5d39725b56331d4361fec90
sbenstewart <sbenstewart@gmail.com>
2019-04-26 20:06:37 +0530

    could not do it

--- js/main.js
+++ js/main.js
@@ -8,6 +8,11 @@ jQuery(document).ready(function($) {
 
 	"use strict";
 
+<<<<<<< HEAD
+=======
+	
+
+>>>>>>> parent of 0e01424... pwa closer
 	var siteMenuClone = function() {
 
 		$('.js-clone-nav').each(function() {
@@ -17,11 +22,11 @@ jQuery(document).ready(function($) {
 
 
 		setTimeout(function() {
-
+			
 			var counter = 0;
       $('.site-mobile-menu .has-children').each(function(){
         var $this = $(this);
-
+        
         $this.prepend('<span class="arrow-collapse collapsed">');
 
         $this.find('.arrow-collapse').attr({
@@ -47,8 +52,8 @@ jQuery(document).ready(function($) {
       } else {
         $this.addClass('active');
       }
-      e.preventDefault();
-
+      e.preventDefault();  
+      
     });
 
 		$(window).resize(function() {
@@ -73,7 +78,7 @@ jQuery(document).ready(function($) {
 				$('body').addClass('offcanvas-menu');
 				$this.addClass('active');
 			}
-		})
+		}) 
 
 		// click outisde offcanvas
 		$(document).mouseup(function(e) {
@@ -84,7 +89,7 @@ jQuery(document).ready(function($) {
 				}
 	    }
 		});
-	};
+	}; 
 	siteMenuClone();
 
 
@@ -224,7 +229,7 @@ jQuery(document).ready(function($) {
 		    + '<span class="countdown-block"><span class="label">%M</span> min </span>'
 		    + '<span class="countdown-block"><span class="label">%S</span> sec</span>'));
 		});
-
+				
 	};
 	siteCountDown();
 
