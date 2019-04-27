a3b61ee8c121450df296f52dcb1e167b8dedf1b1
sbenstewart <sbenstewart@gmail.com>
2019-04-27 09:50:48 +0530

    sw

--- js/sw.js
+++ js/sw.js
@@ -1,10 +1,31 @@
-/** An empty service worker! */
-self.addEventListener('fetch', function(event) {
-  /** An empty fetch handler! */
+importScripts('/cache-polyfill.js');
+
+
+self.addEventListener('install', function(e) {
+ e.waitUntil(
+   caches.open('airhorner').then(function(cache) {
+     return cache.addAll([
+       '/',
+       '/index.html',
+       '/index.html?homescreen=1',
+       '/?homescreen=1'
+     ]);
+   })
+ );
 });
-self.addEventListener('push', function(event) {
-  event.waitUntil(
-    self.registration.showNotification('Got Push?', {
-      body: 'Push Message received'
-   }));
+
+self.addEventListener('fetch', function(event) {
+
+console.log(event.request.url);
+
+event.respondWith(
+
+caches.match(event.request).then(function(response) {
+
+return response || fetch(event.request);
+
+})
+
+);
+
 });
