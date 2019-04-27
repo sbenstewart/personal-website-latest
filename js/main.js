60803b3ca6d0df65d693b45e4a199ddeea085218
sbenstewart <sbenstewart@gmail.com>
2019-04-27 15:29:24 +0530

    best practices

--- js/main.js
+++ js/main.js
@@ -238,3 +238,18 @@ jQuery(document).ready(function($) {
 	siteDatePicker();
 
 });
+
+jQuery.event.special.touchstart =
+    {
+      setup: function( _, ns, handle )
+      {
+        if ( ns.includes("noPreventDefault") )
+        {
+          this.addEventListener("touchstart", handle, { passive: false });
+        }
+        else
+        {
+          this.addEventListener("touchstart", handle, { passive: true });
+        }
+      }
+    };
