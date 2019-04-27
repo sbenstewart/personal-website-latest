cef1b25b78cec20dbbba3e077ea8ba499c47f799
sbenstewart <sbenstewart@gmail.com>
2019-04-27 13:02:40 +0530

    js cache loaded

--- sw.js
+++ sw.js
@@ -16,7 +16,19 @@ self.addEventListener('install', function(e) {
        'css/bootstrap-datepicker.css',
        'fonts/flaticon/font/flaticon.css',
        'css/aos.css',
-       'css/style.css'
+       'css/style.css',
+       'js/jquery-3.3.1.min.js',
+       'js/jquery-migrate-3.0.1.min.js',
+       'js/jquery-ui.js',
+       'js/popper.min.js',
+       'js/bootstrap.min.js',
+       'js/owl.carousel.min.js',
+       'js/jquery.stellar.min.js',
+       'js/jquery.countdown.min.js',
+       'js/jquery.magnific-popup.min.js',
+       'js/bootstrap-datepicker.min.js',
+       'js/aos.js',
+       'js/main.js'
      ]);
    })
  );
